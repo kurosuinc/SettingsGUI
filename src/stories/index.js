40a41d621726f40c15a76cabce93488b25da17c2
ONOUE Origami <yonosamochi@me.com>
2018-06-08 05:37:20 +0900

    storiesをコンポーネントごとに分割

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-
-import schemaSampleData from '../testdata/schemaSample';
-
-import TextField from '../Atom/TextField.jsx';
-
-storiesOf('Settings Import Field', module)
-    .add('basic', () => <TextField schema={schemaSampleData.schema[0]}/>);
+import '../Atom/stories';
