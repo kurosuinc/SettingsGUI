5331bc073734caa46386d9c277b616e982c20069
on_origami <yonosamochi@me.com>
2018-06-08 05:09:10 +0900

    jsxにしたことによりimport errorが発生するのを解消

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
