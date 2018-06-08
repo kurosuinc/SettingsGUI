692260d716ff77c31f66063953dcafd855c0af30
on_origami <yonosamochi@me.com>
2018-06-08 17:41:43 +0900

    必要と思われるReduxのconstantとactionを定義

--- src/Redux/action/index.js
+++ src/Redux/action/index.js
@@ -1,3 +1,12 @@
 // @flow
 import { createActions } from 'redux-actions';
 import * as types from '../constant';
+
+export const appActions = createActions(
+	types.APP.REQUEST_LOAD_SCHEMA_FILE,
+	types.FILE.REQUEST_EXPORT_FILE,
+);
+
+export const fieldActions = createActions(
+	types.RULE_CHECK.REQUEST_RULE_CHECK,
+);
