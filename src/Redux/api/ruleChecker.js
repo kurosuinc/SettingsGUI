bd85f2a51480a078a2b4fe2617be2d545325c8ba
on_origami <yonosamochi@me.com>
2018-06-11 02:19:02 +0900

    ruleCheck関数を実装

---
+++ src/Redux/api/ruleChecker.js
@@ -0,0 +1,11 @@
+// @flow
+import type {rule} from "../../types/rule";
+
+export const ruleCheck = (rules: Array<rule>, text: string | number): ?rule => (
+    rules[rules.map((item: rule): boolean => text.toString().replace(RegExp(item.regex), '') === '').indexOf(false)]
+); // toStringするのはどうなの…
+
+export default (rules: Array<rule>, text: string): ?string => {
+    const result = ruleCheck(rules, text);
+    return result ? result.text : undefined;
+}
