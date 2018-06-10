15c3c12e4de3838b871eb21fd3a708de52fa50a3
ONOUE Origami <yonosamochi@me.com>
2018-06-11 04:37:28 +0900

    sagaのテンプレを作成

--- src/Redux/saga/index.js
+++ src/Redux/saga/index.js
@@ -1,8 +1,15 @@
 // @flow
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, takeLatest } from 'redux-saga/effects';
 import * as types from '../constant';
 
+import loadSchemaFile from './loadSchemaFile';
+import exportConfigFile from './exportConfigFile';
+import ruleCheck from './ruleCheck';
+
 function* rootSaga(): any {
-}
+    takeEvery(types.RULE_CHECK.REQUEST_RULE_CHECK, ruleCheck);
+    takeLatest(types.APP.REQUEST_LOAD_SCHEMA_FILE, loadSchemaFile);
+    takeLatest(types.FILE.REQUEST_EXPORT_FILE, exportConfigFile)
+};
 
 export default rootSaga;
---
+++ src/Redux/saga/loadSchemaFile.js
@@ -0,0 +1,7 @@
+// @flow
+import { put, call, select } from 'redux-saga/effects';
+import * as types from '../constant';
+
+export default function*(action) {
+
+};
