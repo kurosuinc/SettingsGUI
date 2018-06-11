1076c738ae4d2f8ff66f2dd58ca60b17da78cea5
ONOUE Origami <yonosamochi@me.com>
2018-06-11 10:01:28 +0900

    Redux/saga/indexの各文にyieldを付け忘れていたのを修正

--- src/Redux/saga/index.js
+++ src/Redux/saga/index.js
@@ -7,9 +7,9 @@ import exportConfigFile from './exportConfigFile';
 import ruleCheck from './ruleCheck';
 
 function* rootSaga(): any {
-    takeEvery(types.RULE_CHECK.REQUEST_RULE_CHECK, ruleCheck);
-    takeLatest(types.APP.REQUEST_LOAD_SCHEMA_FILE, loadSchemaFile);
-    takeLatest(types.FILE.REQUEST_EXPORT_FILE, exportConfigFile)
-};
+    yield takeEvery(types.RULE_CHECK.REQUEST_RULE_CHECK, ruleCheck);
+    yield takeLatest(types.APP.REQUEST_APP_INITIALIZE, loadSchemaFile);
+    yield takeLatest(types.FILE.REQUEST_EXPORT_FILE, exportConfigFile);
+}
 
 export default rootSaga;
