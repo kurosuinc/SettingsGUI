6b4c98dd66ba5e9948fee81252583903f475122e
ONOUE Origami <yonosamochi@me.com>
2018-06-11 10:11:24 +0900

    Main, Schema reducerをReduxに登録

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -1,10 +1,14 @@
 // @flow
 import {combineReducers} from 'redux';
 
+import Main from './Main';
+import Schema from './Schema';
 import Field from './Field';
 import Notification from './Notification';
 
 export default combineReducers({
+    Main,
+    Schema,
 	Field,
 	Notification,
 });
