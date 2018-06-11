52fd969490c07ae173247bd308f7de47c8399464
ONOUE Origami <yonosamochi@me.com>
2018-06-11 10:25:57 +0900

    FIELD_CREATED アクションを登録し、Field reducerに初期化の概念を導入

--- src/Redux/reducer/Field.js
+++ src/Redux/reducer/Field.js
@@ -5,6 +5,18 @@ import type { field } from '../../types/field';
 
 export type State = Array<field>;
 
-const reducer = handleActions({}, initState);
+const initState: State = [];
+
+const reducer = handleActions({
+    [types.RULE_CHECK.RULE_CHECK_SUCCEEDED]: (state: State, action: Object) => (
+        state // TODO
+    ),
+    [types.RULE_CHECK.RULE_CHECK_FAILED]: (state: State, action: Object) => (
+        state // TODO
+    ),
+    [types.APP.FIELD_CREATED]: (state: State, action: Object) => (
+        action.payload.fields
+    )
+}, initState);
 
 export default reducer;
