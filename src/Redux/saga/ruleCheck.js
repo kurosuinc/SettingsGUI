d021724eb3b588b7a54c92d30356a5ccf4415ec8
on_origami <yonosamochi@me.com>
2018-06-12 20:18:46 +0900

    saga/ruleCheckを実装

--- src/Redux/saga/ruleCheck.js
+++ src/Redux/saga/ruleCheck.js
@@ -1,14 +1,35 @@
 // @flow
 import { put, call, select } from 'redux-saga/effects';
 import * as types from '../constant';
+import rulechecker from '../api/ruleChecker';
 
 export default function*(action: Object): any {
     try{
+        const {target, index} = action.payload;
         yield put({
             type: types.RULE_CHECK.UPDATE_FORM_TEXT,
             payload: action.payload
         });
+
+        const rules = yield select((state: Object) => state.Schema[index].rules);
+        const result = rulechecker(rules, target);
+        if(result){
+            yield put({
+                type: types.RULE_CHECK.RULE_CHECK_SUCCEEDED,
+                payload: {
+                    index
+                }
+            });
+        }else{ //
+            yield put({
+                type: types.RULE_CHECK.RULE_CHECK_FAILED,
+                payload: {
+                    index,
+                    error: result,
+                }
+            });
+        }
     }catch(e){
-        console.log(e);
+        throw e;
     }
 };
