d693a513c5ff31f41cbe56a145fbbd98c0dbdfd9
ONOUE Origami <onoueorigami@gmail.com>
2018-07-10 13:39:47 +0900

    ItemListコンポーネントの大きさと色を指定

--- src/Component/Organism/ItemList.jsx
+++ src/Component/Organism/ItemList.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import { pure } from 'recompose';
 import List from '@material-ui/core/List';
 import Row from '../Molecule/Row';
 import type {spreadField} from "../../types/field";
+
+const Styled = {
+    Root: styled(List)`
+        height: 100%;
+        overflow: auto;
+        background-color: white;
+    `,
+};
 
 type Props = {
     fields: Array<spreadField>,
@@ -10,11 +19,11 @@ type Props = {
 };
 
 const ItemList = (props: Props) => (
-    <List>
+    <Styled.Root>
         {props.fields.map((item, index) =>
             <Row key={item.key} index={index} field={item} checkText={props.checkText}/>
         )}
-    </List>
+    </Styled.Root>
 );
 
 export default pure(ItemList);
